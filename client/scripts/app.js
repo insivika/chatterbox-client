1e422eb894bdd74fd44c4b813e6d8ddeb2738b5f
Bijan Albrecht <Bijanalbrecht@PAlbrecht-W10L.mmreibc.prv>
2018-07-29 10:28:29 -0700

    Enabled POST and manipulating the DOM with message divs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,92 @@
-// YOUR CODE HERE:
+var app = {
 
+  init : function(){
 
-let message = {
-    username: 'shawndrost',
-    text: 'trololo',
-    roomname: '4chan'
-  };
+  },
+  send: function(){
+
+  },
+
+  receive: function(){
+
+  }
+}
+
 
 $(document).ready(function(){
-    $("#messenger-form").submit(function(e){
-       
-    
 
-    e.preventDefault()
-  });
+    var $chats = $('#chats');
+
+
+    let message = {
+      username: 'shawndrost',
+      text: 'Cool beans',
+      roomname: '4chan',
+    };
+
+    // SEND FUNCTION
+    app.send = $("#messenger-form").submit(function(e){
+      
+      message.text = $('.messenger-input').val();
+
+      console.log(message.text)
+
+      $.ajax({
+        url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/message',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function (chats) {
+          console.log('chatterbox: Message sent');
+    
+          $chats.append('<div><strong>' + message.username + '</strong><br>' + message.text +'</div>');
+    
+    
+          // <div>
+          //   <strong>Name:</strong> 
+          //   <br>
+          //   Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          // </div>
+        },
+        error: function (data) {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to send message', data);
+        }
+      });
+    
+      e.preventDefault()
+      });
+
+
+      app.receive = function(){
+        $.ajax({
+          // This is the url you should use to communicate with the parse API server.
+          url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/message',
+          type: 'GET',
+          data: JSON.stringify(message),
+          contentType: 'application/json',
+          success: function (data) {
+            console.log('chatterbox: Message sent');
+          },
+          error: function (data) {
+            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+            console.error('chatterbox: Failed to send message', data);
+          }
+        });
+      }
+    
+  
+
+
+
+
 });
 
 
-//send
+//POST MESSAGES
 
-$.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  });
 
-  
+
+
+  // RETRIEVE MESSAGES
+
