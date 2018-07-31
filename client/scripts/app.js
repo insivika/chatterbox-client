ea2b3a2d1a52913b5257ba586409b20f09a7c406
Josh Herz <herz.josh@gmail.com>
2018-07-30 20:35:43 -0700

    edit

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,37 +1,141 @@
-// YOUR CODE HERE:
+var app = {
+
+  rooms : {
+    lobby: 'lobby'
+  },
+
+};
 
 
 let message = {
-    username: 'shawndrost',
-    text: 'trololo',
-    roomname: '4chan'
-  };
+  username: 'shawndrost',
+  text: 'Cool beans',
+  roomname: '4chan',
+};
+
+
+app.init =  function(){
 
 $(document).ready(function(){
-    $("#messenger-form").submit(function(e){
-       
-    
 
-    e.preventDefault()
-  });
+    var $chats = $('#chats');
+
+    // Setting input requirements
+    $(".messenger-input").prop('required',true);
+    $("#add-room-input").prop('required',true);
+
+
+    // Add room to DOM
+
+    $("#add-room").submit(function(e){
+
+      var newRoom = $('#add-room-input').val();
+
+
+      $('#room-selector').append('<option>' + newRoom + '</option>')
+
+      console.log(newRoom);
+
+      $('#add-room-input').val('');
+
+      e.preventDefault()
+    })
+
+
+$("#messenger-form").submit(function(e){
+
+  e.preventDefault()
+
+  // Re-assigning keys to the message object
+
+  message.text = $('.messenger-input').val();
+
+  var user = window.location.search;
+
+  message.username = user.split('=')[1]
+
+  message.roomname = $('#room-selector').val();
+
+  app.send();
+
+  $('.messenger-input').val('');
+
 });
 
 
-//send
 
-$.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
+
+// SEND FUNCTION
+app.send = function(message){
+
+        // POST message to server
+
+        $.ajax({
+          url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
+          type: 'POST',
+          data: JSON.stringify(message),
+          contentType: 'application/json',
+          success: function (chats) {
+            console.log('chatterbox: Message sent');
+
+          },
+          error: function (data) {
+            // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+            console.error('chatterbox: Failed to send message', data);
+          }
+        });
+
     }
-  });
 
-  
+
+setInterval(app.fetch,1000);
+
+  // GET messages from server
+
+  app.fetch = function(message){
+     $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      dataType: 'jsonp',
+      contentType: 'application/json',
+
+
+
+      success: function (messages) {
+
+        $.each(messages.results, function(i, message){
+
+          $chats.append('<div><strong>'+ message.username +':</strong><br>'+message.text+'</div>');
+
+          //console.log(message)
+
+
+            if(app.rooms[message.roomname] !== message.roomname){
+
+              app.rooms[message.roomname] = message.roomname;
+
+            }
+
+        });
+        console.log('chatterbox: Message received');
+
+
+        for(key in app.rooms){
+          $('#room-selector').append('<option>' + app.rooms[key] + '</option>')
+        }
+
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to receive message', data);
+      }
+
+    });
+
+  }  
+});
+
+};
+
+app.init();
